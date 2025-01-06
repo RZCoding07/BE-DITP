e747316cafa260dff71516099b6d94e4f313d969
RZCoding07 <muhammadrizkylubis@students.polmed.ac.id>
2025-01-06 16:28:12 +0700

    fix: comment out success connection log in initializeDatabase function

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ const initializeDatabase = async () => {
     try {
         await db_app.authenticate(); // Test connection
         await db_app.sync({ alter: true }); // Sync models with database
-        console.log('Connection has been established successfully.');
+        // console.log('Connection has been established successfully.');
     } catch (error) {
         console.error('Unable to connect to the database:', error);
     }
