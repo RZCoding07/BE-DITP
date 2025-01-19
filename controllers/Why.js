a88097bc192ec204c01c65d2bdca67ae3e41c2ff
RZCoding07 <muhammadrizkylubis@students.polmed.ac.id>
2025-01-19 22:57:17 +0700

    refactor: improve caching logic in getAllWhy to enhance performance and reduce database queries

--- controllers/Why.js
+++ controllers/Why.js
@@ -6,25 +6,25 @@ const cache = new NodeCache({ stdTTL: 100, checkperiod: 120 });
 
 export const getAllWhy = async (req, res) => {
     try {
-        const key = 'why';
-        let why = cache.get(key);
-
-        if (why) {
-            return res.status(200).json(why);
-        } else {
-
-            const sqlQuery = "SELECT * FROM vw_pi";
-
-            const result = await db_app.query(sqlQuery);
-
-            cache.set(key, result);
-
-            res.status(200).json(result);
+        // Cek apakah data ada di cache
+        const cachedData = cache.get('AllWhy');
+        if (cachedData) {
+            console.log("Cache hit for AllWhy");
+            return res.status(200).json(cachedData);
         }
 
+        // Ambil data dari database jika tidak ada di cache
+        const why = await db_app.query('SELECT * FROM vw_pi', { type: db_app.QueryTypes.SELECT });
 
+
+    
+        
+        // Simpan hasil query ke cache
+        cache.set('AllWhy', why);
+
+        res.status(200).json(why);
     } catch (error) {
-        // Handle any errors that occur during the process
         res.status(500).json({ message: error.message });
     }
 };
+
